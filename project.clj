0656244dcd7c873de64ca852564d619bbc8a85c4
Erwin Rooijakkers <erwin@rooijakkers.software>
2020-07-28 20:28:24 +0200

    Bump versions

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject nl.mediquest/duct.module.reitit "1.0.0"
+(defproject nl.mediquest/duct.module.reitit "1.0.1"
   :description "A Reitit router for Duct"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [metosin/reitit "0.3.10"]
-                 [duct/core "0.7.0"]
-                 [integrant "0.7.0"]])
+                 [metosin/reitit "0.5.5"]
+                 [duct/core "0.8.0"]
+                 [integrant "0.8.0"]])
