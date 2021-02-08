270932a7028f12e055eea4219d3995b888d1b5b0
Erwin Rooijakkers <erwin@rooijakkers.software>
2021-02-08 11:34:03 +0100

    Bump dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject nl.mediquest/duct.module.reitit "1.0.2"
+(defproject nl.mediquest/duct.module.reitit "1.0.3"
   :description "A Reitit router for Duct"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [metosin/reitit "0.5.5"]
+  :dependencies [[org.clojure/clojure "1.10.2"]
+                 [metosin/reitit "0.5.12"]
                  [duct/core "0.8.0"]
                  [integrant "0.8.0"]])
