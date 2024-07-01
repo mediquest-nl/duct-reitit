d82db0c84a7ea169d928415409a73b560646a083
Erwin Rooijakkers <erwin@rooijakkers.software>
2024-07-01 14:35:21 +0200

    Bump reitit

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.3"]
-                 [metosin/reitit "0.7.0"]
+                 [metosin/reitit "0.7.1"]
                  [duct/core "0.8.1"]
                  [integrant "0.10.0"]])
